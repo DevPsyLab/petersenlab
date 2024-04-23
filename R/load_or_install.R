62f9e28d5618397573cdf8002468b0cb85577c62
Petersen <itpetersen@uiowa.edu>
2024-04-23 12:31:49 -0500

    20240423 - update packages and license

--- R/load_or_install.R
+++ R/load_or_install.R
@@ -9,6 +9,7 @@
 #' already installed, it installs and then loads them.
 #'
 #' @param package_names Character vector of one or more package names.
+#' @param ... Additional arguments for \code{install.packages()}.
 #'
 #' @return
 #' Loaded packages.
@@ -27,10 +28,10 @@
 #' @seealso
 #' \url{https://www.r-bloggers.com/2012/05/loading-andor-installing-packages-programmatically/}
 
-load_or_install <- function(package_names){
+load_or_install <- function(package_names, ...){
   for(package_name in package_names){
     if(!is_installed(package_name)){
-      install.packages(package_name)
+      install.packages(package_name, ...)
     }
     library(package_name, character.only = TRUE, quietly = TRUE, verbose = FALSE)
   }
