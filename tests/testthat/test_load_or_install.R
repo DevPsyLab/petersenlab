3694e5c6c87affbb45ad451e9e43a39e1fdcfaa7
Petersen <itpetersen@uiowa.edu>
2024-04-24 06:59:56 -0500

    20240424 - working test

--- tests/testthat/test_load_or_install.R
+++ tests/testthat/test_load_or_install.R
@@ -1,36 +1,32 @@
 library(testthat)
+library(withr)
 
-# Define a test for load_or_install function
-test_that("load_or_install installs and loads packages correctly", {
-  # Set up a temporary library directory
-  temp_lib <- tempdir()
-  old_libs <- .libPaths() # Save current library paths
-  .libPaths(temp_lib)
+# Set up a temporary library directory
+temp_lib <- tempdir()
+old_libs <- .libPaths() # Save current library paths
+.libPaths(c(temp_lib, .libPaths()))
 
-  # Run load_or_install function
-  oldpath <- getwd()
-  filelocation <- system.file(
-    "extdata",
-    "testpackage1_0.1.0.tar.gz",
-    package = "petersenlab")
-  filepath <- dirname(filelocation)
-  newpath <- setwd(filepath)
-  setwd(newpath)
-  load_or_install(
-    c("testpackage1", "testpackage2"),
-    repos = NULL,
-    type = "source")
+# Run load_or_install() function
+oldpath <- getwd()
+filelocation <- system.file(
+  "extdata",
+  "testpackage1_0.1.0.tar.gz",
+  package = "petersenlab")
+filepath <- dirname(filelocation)
+newpath <- filepath #setwd(filepath)
+setwd(newpath)
 
-  # Check if packages are installed and loaded correctly
-  expect_true("testpackage1" %in% installed.packages())
-  expect_true(requireNamespace("testpackage1", quietly = TRUE))
-  expect_true("testpackage2" %in% installed.packages())
-  expect_true(requireNamespace("testpackage2", quietly = TRUE))
+load_or_install(
+  c("testpackage1", "testpackage2"),
+  repos = NULL,
+  type = "source")
 
-  # Clean up
-  .libPaths(old_libs)  # Restore original library paths
-  setwd(oldpath)
-})
+# Check if packages are installed and loaded correctly
+expect_true("testpackage1" %in% installed.packages())
+expect_true(requireNamespace("testpackage1", quietly = TRUE))
+expect_true("testpackage2" %in% installed.packages())
+expect_true(requireNamespace("testpackage2", quietly = TRUE))
 
-# Run the tests
-test_load_or_install()
+# Clean up
+.libPaths(old_libs)  # Restore original library paths
+setwd(oldpath)
