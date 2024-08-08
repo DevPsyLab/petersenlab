a908a19797a0928bce0025c4ae73578a04531a69
Isaac T. Petersen <isaac-t-petersen@uiowa.edu>
2024-08-07 23:06:48 -0500

    20240807 - add examples

--- R/weightedQuantile.R
+++ R/weightedQuantile.R
@@ -24,6 +24,8 @@
 #'
 #' @examples
 #' mydata <- c(1:100, 1000)
+#' mydataWithNAs <- mydata
+#' mydataWithNAs[c(1,5,7)] <- NA
 #' weights <- rep(1, length(mydata))
 #' quantiles <- c(0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99)
 #'
@@ -39,6 +41,21 @@
 #'
 #' wquantile(
 #'   x = mydata,
+#'   w = weights,
+#'   probs = quantiles)
+#'
+#' whdquantile(
+#'   x = mydataWithNAs,
+#'   w = weights,
+#'   probs = quantiles)
+#'
+#' wthdquantile(
+#'   x = mydataWithNAs,
+#'   w = weights,
+#'   probs = quantiles)
+#'
+#' wquantile(
+#'   x = mydataWithNAs,
 #'   w = weights,
 #'   probs = quantiles)
 #'
